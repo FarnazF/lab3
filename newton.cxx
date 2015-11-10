bd834965d2c17761e16c934e4fd37d1c18d1de9e
FarnazF <farnaz.farzam@uni-duesseldorf.de>
2015-11-10 17:05:15 +0100

    Update newton.cxx

--- newton.cxx
+++ newton.cxx
@@ -1,13 +1,27 @@
 #include <iostream>
 #include <complex>
+using namespace std;
 
 int main(){
-
-  // Example how to declare a single complex variable:
-  complex<double> c;
-
-  // d = 1 + 2i 
-  complex<double> d = complex<double>(1.0, 2.0); 
-
+  complex<double> z0;
+  complex<double> z1;
+  for(double re=-2; re<=2; re+=0.01){
+    for(double im=-2; im<=2; im+=0.01){
+       complex<double> z0= complex<double>(re,im);
+   int i;
+      for(i=0; i<=40; i++){
+      z1=z0;
+      z0=z0-((z0*z0*z0-1.0)/(3.0*z0*z0));
+      if (abs(z1-z0)< 1e-8) break;
+      
+      }
+   
+   cout << re <<"\t"<< im<< "\t"<< i << endl;
+      
+    }
+    
+  }
+   
+      
   return 0;
 }
